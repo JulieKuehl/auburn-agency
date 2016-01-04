6e90211c2e19a148d8999e95a28b0f5544aeb7e5
juliekuehl <julie@juliekuehl.com>
2016-01-04 13:30:53 -0600

    footer widget icons

--- themes/auburnagency/footer.php
+++ themes/auburnagency/footer.php
@@ -13,8 +13,29 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="footer-container">
+			<div class="fat-footer">
+				<? dynamic_sidebar( 'fat-footer-one' ); ?>
+				<!-- Address icon -->
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle-thin fa-stack-2x"></i>
+					<i class="fa fa-map-marker fa-stack-1x"></i>
+				</span>
+				<? dynamic_sidebar( 'fat-footer-two' ); ?>
+				<!-- Phone icon -->
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle-thin fa-stack-2x"></i>
+					<i class="fa fa-phone fa-stack-1x"></i>
+				</span>
+				<? dynamic_sidebar( 'fat-footer-three' ); ?>
+				<!-- Email icon -->
+				<span class="fa-stack fa-3x">
+					<i class="fa fa-circle-thin fa-stack-2x"></i>
+					<i class="fa fa-envelope fa-stack-1x"></i>
+				</span>
+				<? dynamic_sidebar( 'fat-footer-four' ); ?>
+			</div>
 			<div class="site-info">
-				<?php printf( __( 'Forward by %1$s &mdash; A %2$s Project.', 'auburnagency' ), '<a href="http://design.org" >Design.org</a>', '<a href="http://drawbackwards.com" rel="designer">Drawbackwards</a>' ); ?>
+				&copy; <?php date( 'Y' ); ?> Auburn Agency Inc. Crop Insurance. Site powered and designed by <a href="http://bcomonline.com">BCom Solutions, LLC.</a>
 			</div><!-- .site-info -->
 		</div>
 	</footer><!-- #colophon -->
