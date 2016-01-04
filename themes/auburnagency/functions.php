b702b7ed3ca1eb390a4f039fe65df6d9ddc35762
juliekuehl <julie@juliekuehl.com>
2016-01-04 12:56:47 -0600

    changed 'forward' to 'auburnagency'

--- themes/auburnagency/functions.php
+++ themes/auburnagency/functions.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Forward functions and definitions
+ * Auburnagency functions and definitions
  *
  * @package auburnagency
  */
 
 
-if ( ! function_exists( 'forward_google_fonts' ) ) :
+if ( ! function_exists( 'auburnagency_google_fonts' ) ) :
 /**
  * Adds google font support.
  * 
  */
-function forward_google_fonts() {
+function auburnagency_google_fonts() {
 	$query_args = array(
 		'family' => 'Source+Sans+Pro:200,300,400,600',
 
@@ -25,8 +25,8 @@ function forward_google_fonts() {
 
 	wp_enqueue_style('source-sans');
 }
-endif; // forward_google_fonts
-add_action('wp_enqueue_scripts', 'forward_google_fonts');
+endif; // auburnagency_google_fonts
+add_action('wp_enqueue_scripts', 'auburnagency_google_fonts');
 
 /**
  * Set the content width based on the theme's design and stylesheet.
@@ -36,7 +36,7 @@ if ( ! isset( $content_width ) ) {
 }
 
 
-if ( ! function_exists( 'forward_setup' ) ) :
+if ( ! function_exists( 'auburnagency_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -44,7 +44,7 @@ if ( ! function_exists( 'forward_setup' ) ) :
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function forward_setup() {
+function auburnagency_setup() {
 
 	/*
 	 * Make theme available for translation.
@@ -94,22 +94,22 @@ function forward_setup() {
 	) );
 
 	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'forward_custom_background_args', array(
+	add_theme_support( 'custom-background', apply_filters( 'auburnagency_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
 }
-endif; // forward_setup
-add_action( 'after_setup_theme', 'forward_setup' );
+endif; // auburnagency_setup
+add_action( 'after_setup_theme', 'auburnagency_setup' );
 
 
-if ( ! function_exists( 'forward_widgets_init' ) ) :
+if ( ! function_exists( 'auburnagency_widgets_init' ) ) :
 /**
  * Register widget area.
  *
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
-function forward_widgets_init() {
+function auburnagency_widgets_init() {
 	register_sidebar( array(
 		'name'          => __( 'Sidebar', 'auburnagency' ),
 		'id'            => 'sidebar-1',
@@ -120,15 +120,15 @@ function forward_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 }
-endif; // forward_widgets_init
-add_action( 'widgets_init', 'forward_widgets_init' );
+endif; // auburnagency_widgets_init
+add_action( 'widgets_init', 'auburnagency_widgets_init' );
 
-if ( ! function_exists( 'forward_scripts' ) ) :
+if ( ! function_exists( 'auburnagency_scripts' ) ) :
 /**
  * Enqueue scripts and styles.
  */
-function forward_scripts() {
-	wp_enqueue_style( 'forward-style', get_stylesheet_uri() );
+function auburnagency_scripts() {
+	wp_enqueue_style( 'auburnagency-style', get_stylesheet_uri() );
 
 	// Front-end scripts
 	if ( !is_admin() ) {
@@ -141,7 +141,7 @@ function forward_scripts() {
 	  }
 
 	  // Load theme-specific JavaScript with versioning based on last modified time; http://www.ericmmartin.com/5-tips-for-using-jquery-with-wordpress/
-	  wp_enqueue_script( 'forward-js-core', get_stylesheet_directory_uri() . '/js/core' . $suffix . '.js', array( 'jquery' ), filemtime( get_template_directory() . '/js/core' . $suffix . '.js' ), true );
+	  wp_enqueue_script( 'auburnagency-js-core', get_stylesheet_directory_uri() . '/js/core' . $suffix . '.js', array( 'jquery' ), filemtime( get_template_directory() . '/js/core' . $suffix . '.js' ), true );
 
 	  // Conditionally load another script
 	  // if ( is_singular() ) {
@@ -149,16 +149,16 @@ function forward_scripts() {
 	  // }
 	}
 
-	// wp_enqueue_script( 'forward-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	// wp_enqueue_script( 'auburnagency-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	// wp_enqueue_script( 'forward-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	// wp_enqueue_script( 'auburnagency-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-endif; // forward_scripts
-add_action( 'wp_enqueue_scripts', 'forward_scripts' );
+endif; // auburnagency_scripts
+add_action( 'wp_enqueue_scripts', 'auburnagency_scripts' );
 
 
 /**
