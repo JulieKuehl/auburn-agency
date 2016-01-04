e5c12b72f51f7a345750270589d106d42312a4ec
juliekuehl <julie@juliekuehl.com>
2016-01-04 15:23:04 -0600

    added site logo

--- themes/auburnagency/header.php
+++ themes/auburnagency/header.php
@@ -24,8 +24,9 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-container">
 			<div class="site-branding">
-				<h1 class="site-title"><a class="site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<div class="site-description"><?php bloginfo( 'description' ); ?></div>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/auburn-agency-logo.png" alt="<?php bloginfo( 'name' ); ?>" />
+<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
+<!--				<div class="site-description">--><?php //bloginfo( 'description' ); ?><!--</div>-->
 			</div><!-- .site-branding -->
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 				<div class="container">
